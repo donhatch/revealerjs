e101e6c47c148c757fb67392870f726653088b47
Don Hatch <hatch@plunk.org>
2016-12-12 23:21:36 -0800

    revealer is super nice now! does all 2-child and 4-child configs.

--- revealer.js
+++ revealer.js
@@ -24,29 +24,53 @@
 let setUpRevealer = function(container) {
   let verboseLevel = 0;
   if (verboseLevel >= 1) console.log("    in setUpRevealer(container=",container,")");
-  let handles = container.getElementsByClassName("revealer-handle");
-  if (handles.length == 1) {
-    if (verboseLevel >= 1) console.log("      handles=",handles);
-    let handle = handles[0];
 
-    // XXX TODO: handle and sanity check all cases:
-    //        W E
-    //        N S
-    //        NW NE S
-    //        SE SW N
-    //        NW SW E
-    //        NE SE W
-    //        NW NE SW SE
-    let childN = container.getElementsByClassName("revealer-child-N")[0];
-    let childS = container.getElementsByClassName("revealer-child-S")[0];
-    let childW = container.getElementsByClassName("revealer-child-W")[0];
-    let childE = container.getElementsByClassName("revealer-child-E")[0];
-    let childNW = container.getElementsByClassName("revealer-child-NW")[0];
-    let childSW = container.getElementsByClassName("revealer-child-SW")[0];
-    let childNE = container.getElementsByClassName("revealer-child-NE")[0];
-    let childSE = container.getElementsByClassName("revealer-child-SE")[0];
+  // TODO: handle and sanity check all common configurations:
+  //        W E
+  //        N S
+  //        NW NE S
+  //        SE SW N
+  //        NW SW E
+  //        NE SE W
+  //        NW NE SW SE
+  let childW = container.getElementsByClassName("revealer-child-W")[0];
+  let childE = container.getElementsByClassName("revealer-child-E")[0];
+  let childN = container.getElementsByClassName("revealer-child-N")[0];
+  let childS = container.getElementsByClassName("revealer-child-S")[0];
+  let childNW = container.getElementsByClassName("revealer-child-NW")[0];
+  let childNE = container.getElementsByClassName("revealer-child-NE")[0];
+  let childSW = container.getElementsByClassName("revealer-child-SW")[0];
+  let childSE = container.getElementsByClassName("revealer-child-SE")[0];
+  if (verboseLevel >= 1) console.log("      childW = ",childW);
+  if (verboseLevel >= 1) console.log("      childE = ",childE);
+  if (verboseLevel >= 1) console.log("      childN = ",childN);
+  if (verboseLevel >= 1) console.log("      childS = ",childS);
+  if (verboseLevel >= 1) console.log("      childNW = ",childNW);
+  if (verboseLevel >= 1) console.log("      childNE = ",childNE);
+  if (verboseLevel >= 1) console.log("      childSW = ",childSW);
+  if (verboseLevel >= 1) console.log("      childSE = ",childSE);
 
-    handle.addEventListener("mousedown", event => {
+
+  let toArray = htmlCollection => [].slice.call(htmlCollection);
+
+  let handlesEW = toArray(container.getElementsByClassName("revealer-handle-EW"));
+  let handlesNS = toArray(container.getElementsByClassName("revealer-handle-NS"));
+  let handlesEWhair = toArray(container.getElementsByClassName("revealer-handle-EWhair"));
+  let handlesNShair = toArray(container.getElementsByClassName("revealer-handle-NShair"));
+  let handlesBidirectional = toArray(container.getElementsByClassName("revealer-handle-bidirectional"));
+  let handles = handlesEW.concat(handlesNS).concat(handlesEWhair).concat(handlesNShair).concat(handlesBidirectional);
+
+  let handlesThatMoveEW = handlesEW.concat(handlesEWhair).concat(handlesBidirectional);
+  let handlesThatMoveNS = handlesNS.concat(handlesNShair).concat(handlesBidirectional);
+
+  if (verboseLevel >= 1) console.log("      handles=",handles);
+  for (let handle of handles) {
+    if (verboseLevel >= 1) console.log("          handle=",handle);
+
+    let freeToMoveNS = handlesThatMoveNS.indexOf(handle) != -1;
+    let freeToMoveEW = handlesThatMoveEW.indexOf(handle) != -1;
+
+    let mousedown = function(event) {
       let verboseLevel = 0;
       if (verboseLevel >= 1) console.log("        in mousedown");
       if (verboseLevel >= 1) console.log("          event=",event);
@@ -56,17 +80,26 @@ let setUpRevealer = function(container) {
       let x = x_on_mousedown;
       let y = y_on_mousedown;
       let handle_left_on_mousedown = getComputedStyle(handle, null).left; // in pixels unfortunately
+      let handle_top_on_mousedown = getComputedStyle(handle, null).top; // in pixels unfortunately
       if (verboseLevel >= 1) console.log("          handle_left_on_mousedown = ",handle_left_on_mousedown);
       if (!handle_left_on_mousedown.endsWith('px')) {
         throw new Error("Expected style to end in 'px', got "+JSON.stringify(handle_left_on_mousedown));
       }
+      if (!handle_top_on_mousedown.endsWith('px')) {
+        throw new Error("Expected style to end in 'px', got "+JSON.stringify(handle_top_on_mousedown));
+      }
       let handle_left_pixels_on_mousedown = parseFloat(handle_left_on_mousedown); /* trailing 'px' ignored per doc */
+      let handle_top_pixels_on_mousedown = parseFloat(handle_top_on_mousedown); /* trailing 'px' ignored per doc */
       if (verboseLevel >= 1) console.log("          handle_left_pixels_on_mousedown = ",handle_left_pixels_on_mousedown);
       // TODO: look into possibly using getBoundingClientRect() or getClientRects()
       let container_width_on_mousedown = container.offsetWidth;
+      let container_height_on_mousedown = container.offsetHeight;
       if (verboseLevel >= 1) console.log("          container_width_on_mousedown = ",container_width_on_mousedown);
+      if (verboseLevel >= 1) console.log("          container_height_on_mousedown = ",container_height_on_mousedown);
       let handle_left_percentage_on_mousedown = handle_left_pixels_on_mousedown / container_width_on_mousedown * 100;
+      let handle_top_percentage_on_mousedown = handle_top_pixels_on_mousedown / container_height_on_mousedown * 100;
       if (verboseLevel >= 1) console.log("          handle_left_percentage_on_mousedown = ",handle_left_percentage_on_mousedown);
+      if (verboseLevel >= 1) console.log("          handle_top_percentage_on_mousedown = ",handle_top_percentage_on_mousedown);
       // Add mousemove and mouseup handlers to document,
       // not to handle element
       // (or even to to handle's ancestors, which is what jotform's does).
@@ -102,38 +135,54 @@ let setUpRevealer = function(container) {
           return;
         }
 
+        if ((freeToMoveEW && dx!=0) || (freeToMoveNS && dy!=0)) {
 
-        if (dx != 0) {
-          // Compute new percentage.
+          // Compute new percentages.
           let new_left_pixels = handle_left_pixels_on_mousedown + Dx;
+          let new_top_pixels = handle_top_pixels_on_mousedown + Dy;
           if (verboseLevel >= 1) console.log("                  new_left_pixels = "+new_left_pixels);
+          if (verboseLevel >= 1) console.log("                  new_top_pixels = "+new_top_pixels);
           let new_left_percentage = new_left_pixels / container_width_on_mousedown * 100;
+          let new_top_percentage = new_top_pixels / container_height_on_mousedown * 100;
           if (verboseLevel >= 1) console.log("                  new_left_percentage = "+new_left_percentage);
-          // Clamp new percentage to [0,100].
+          if (verboseLevel >= 1) console.log("                  new_top_percentage = "+new_top_percentage);
+
+          // Clamp new percentages to [0,100].
           let clamp = (x,a,b) => x<=a?a:x>=b?b:x;
           new_left_percentage = clamp(new_left_percentage, 0, 100);
+          new_top_percentage = clamp(new_top_percentage, 0, 100);
           if (verboseLevel >= 1) console.log("                  new_left_percentage = "+new_left_percentage+" (clamped)");
+          if (verboseLevel >= 1) console.log("                  new_top_percentage = "+new_top_percentage+" (clamped)");
 
-
+          //
           // Set the following to it, in percentage rather than pixels
           // (for graceful behavior on resize):
           //   - handle's left
           //   - child-W's clip inset right
           //   - child-E's clip inset left
           // etc.
-          let freeNS = !(childE !== undefined && childW !== undefined); // it's only non-free in that one configuration
-          let freeEW = !(childN !== undefined && childN !== undefined); // it's only non-free in that one configuration
-          if (freeEW) handle.style.left = new_left_percentage+'%';
-          if (freeNS) handle.style.top = new_top_percentage+'%';
+          //
+          if (freeToMoveEW) {
+            for (let otherHandle of handlesThatMoveEW) {
+              otherHandle.style.left = new_left_percentage+'%';
+            }
+            if (childW !== undefined) childW.style.clipPath = "inset(0 "+(100-new_left_percentage)+"% 0 0)";
+            if (childE !== undefined) childE.style.clipPath = "inset(0 0 0 "+new_left_percentage+"%)";
+          }
+          if (freeToMoveNS) {
+            for (let otherHandle of handlesThatMoveNS) {
+              otherHandle.style.top = new_top_percentage+'%';
+            }
+            if (childN !== undefined) childN.style.clipPath = "inset(0 0 "+(100-new_top_percentage)+"% 0)";
+            if (childS !== undefined) childS.style.clipPath = "inset("+new_top_percentage+"% 0 0 0)";
+          }
 
-          if (childN !== undefined) childN.style.clipPath = "inset(0 0 "+(100-new_top_percentage)+"% 0)";
-          if (childE !== undefined) childE.style.clipPath = "inset(0 0 0 "+new_left_percentage+"%)";
-          if (childS !== undefined) childS.style.clipPath = "inset("+new_top_percentage+"%) 0 0 0)";
-          if (childW !== undefined) childW.style.clipPath = "inset(0 "+(100-new_left_percentage)+"% 0 0)";
-          if (childNW !== undefined) childNW.style.clipPath = "inset(0 "+(100-new_left_percentage)+"% "+(100-new_top_percentage)+"% 0)";
-          if (childSE !== undefined) childSE.style.clipPath = "inset("+new_top_percentage+"%) 0 0 "+new_left_percentage+"%)";
-          if (childSW !== undefined) childSW.style.clipPath = "inset("+new_top_percentage+"%) "+(100-new_left_percentage)+"% 0 0)";
-          if (childNE !== undefined) childNE.style.clipPath = "inset(0 0 "+(100-new_top_percentage)+"% "+new_left_percentage+"%)";
+          if (freeToMoveEW && freeToMoveNS) {
+            if (childNW !== undefined) childNW.style.clipPath = "inset(0 "+(100-new_left_percentage)+"% "+(100-new_top_percentage)+"% 0)";
+            if (childSE !== undefined) childSE.style.clipPath = "inset("+new_top_percentage+"% 0 0 "+new_left_percentage+"%)";
+            if (childSW !== undefined) childSW.style.clipPath = "inset("+new_top_percentage+"% "+(100-new_left_percentage)+"% 0 0)";
+            if (childNE !== undefined) childNE.style.clipPath = "inset(0 0 "+(100-new_top_percentage)+"% "+new_left_percentage+"%)";
+          }
         }
 
         if (verboseLevel >= 2) console.log("            out mousemove");
@@ -147,7 +196,9 @@ let setUpRevealer = function(container) {
       document.addEventListener("mousemove", mousemove);
       document.addEventListener("mouseup", mouseup);
       if (verboseLevel >= 1) console.log("        out mousedown");
-    });
+    };
+    handle.addEventListener("mousedown", mousedown);
+
   } // if handles.length == 1
   if (verboseLevel >= 1) console.log("    out setUpRevealer(container=",container,")");
 };  // setUpRevealer
